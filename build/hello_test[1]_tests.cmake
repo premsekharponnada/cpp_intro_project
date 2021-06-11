a2ad278d5370329dd5105e9eeee87fdacfa30d87
PREM SEKHAR <84108418+premsekharponnada@users.noreply.github.com>
2021-06-11 07:41:27 +0530

    removed unwanted files

--- build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( comparestrings.Test1 /Users/premsekhar/Desktop/cppproject/cpp_intro_project/bin/hello_test [==[--gtest_filter=comparestrings.Test1]==] --gtest_also_run_disabled_tests)
-set_tests_properties( comparestrings.Test1 PROPERTIES WORKING_DIRECTORY /Users/premsekhar/Desktop/cppproject/cpp_intro_project/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( hello_test_TESTS comparestrings.Test1)
