6972b1aa33697cd0d9546646b4f732363ffdcf28
PREM SEKHAR <84108418+premsekharponnada@users.noreply.github.com>
2021-06-14 03:02:50 +0530

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/premsekhar/Desktop/cppproject/cpp_intro_project
-# Build directory: /Users/premsekhar/Desktop/cppproject/cpp_intro_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/premsekhar/Desktop/cppproject/cpp_intro_project/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
