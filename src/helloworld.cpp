d43250a99df1532af19471c90e3d03e48c294547
PREM SEKHAR <84108418+premsekharponnada@users.noreply.github.com>
2021-07-01 18:12:29 +0530

    removed dockerfile since using it from dockerhub

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -4,14 +4,3 @@ string welcome()
 {
     return "Hello World";
 }
-void addition()
-{
-    for (int x; x < 10; x++)
-    {
-        for (int y; x < 10; x++)
-        {
-            int count;
-            count = +1;
-        }
-    }
-}
